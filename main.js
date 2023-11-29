27368c6a397c337e197e3dc209645f79828d2c25
Jonas Ploth <151474840+jonasploth@users.noreply.github.com>
2023-11-29 08:05:00 +0100

    Update main.js
    Added a Configuration Section:
    
    A configuration section was introduced at the beginning of the script. Here, basic settings like breakevenRoas, AverageCvr, impressionThreshold, daysAgo, and feedLabels can be adjusted.
    Customizable Product Type Names:
    
    Variables for naming different product types (productTypes) were added within the configuration section. These names are customizable and used in the logic for determining the product type.
    The names for LowImpressions and Poor Performer were swapped.
    Custom Label Selection:
    
    An option to select the Custom Label (selectedCustomLabel) was added to the configuration section. This allows for the selection of a Custom Label ranging from 0 to 4.
    Function to Set Custom Label in Google Sheet:
    
    A function named setCustomLabelInSheet was added. This function sets the selected Custom Label in cell H1 of a specified Google Sheet ("EYL-Izer"). The function uses the existing SPREADSHEET_URL variable to access the spreadshee

--- main.js
+++ main.js
@@ -1,1 +1,125 @@
+// ##################################################
+// Konfigurationsbereich
+// ##################################################
 
+// Auswahl des Custom Labels (0 bis 4)
+var selectedCustomLabel = 0; // Wähle zwischen 0 und 4
+
+// Setzen Sie den Break-even-ROAS
+var breakevenRoas = 6.5;
+
+// Durchschnittliche Conversion Rate
+var AverageCvr = 5;
+
+// Schwellenwert für Impressionen
+var impressionThreshold = 50;
+
+// Zeitspanne für die Datenabfrage (Tage zurück)
+var daysAgo = 90;
+
+// Feed-Labels für die Filterung nach Land und Sprache
+var feedLabels = ['en', 'de', 'us']; // Füge das gewünschte Label hinzu
+
+//Bennenung der Produkttypen (So werden die Custom_Labels heissen
+var productTypeNameOverperformer = 'Machines';
+var productTypeNameGoodPerformer = 'Hustlers';
+var productTypeNameMediumPerformer = 'Sleeper';
+var productTypeNamePoorPerformer = 'killjoy'; 
+var productTypeNameLowImpressions = 'Ninjas'; 
+
+// ##################################################
+// Rest des Skripts (Nichts ändern)
+// ##################################################
+
+function main() {
+    var products = getFilteredShoppingProducts(daysAgo);
+    products.sort(function(a, b) {
+        return a[0] > b[0];
+    });
+    products = products.slice(0, 999999);
+    pushToSpreadsheet(products);
+}
+
+function getFilteredShoppingProducts(daysAgo) {
+    var today = new Date();
+    var daysAgo = new Date(today.getFullYear(), today.getMonth(), today.getDate() - daysAgo);
+    var dateFrom = Utilities.formatDate(daysAgo, AdsApp.currentAccount().getTimeZone(), 'yyyyMMdd');
+    var dateTo = Utilities.formatDate(today, AdsApp.currentAccount().getTimeZone(), 'yyyyMMdd');
+
+    var query =
+        "SELECT OfferId, Impressions, Clicks, Ctr, Cost, Conversions, ConversionValue " +
+        "FROM SHOPPING_PERFORMANCE_REPORT " +
+        "DURING " + dateFrom + "," + dateTo;
+
+    var products = [];
+    var count = 0;
+    var report = AdsApp.report(query);
+    var rows = report.rows();
+    while (rows.hasNext()) {
+        var row = rows.next();
+        var offer_id = formatOfferId(row['OfferId']); // Hier wird die Funktion aufgerufen
+        var impressions = row['Impressions'].toString();
+        var clicks = row['Clicks'].toString();
+        var cost = row['Cost'].toString();
+        var conversions = row['Conversions'].toString();
+        var conversionValue = row['ConversionValue'].toString();
+        var convValuePerCost = (conversionValue.replace(",", "") / cost.replace(",", "")).toString();
+        if (isNaN(convValuePerCost)) {
+            convValuePerCost = 0;
+        }
+
+        var productType = '';
+        if (clicks > (300 / AverageCvr) && convValuePerCost >= breakevenRoas + 1) { 
+        productType = productTypeNameOverperformer; // OVERPERFORMER
+      } else if (clicks >= (100 / AverageCvr) && convValuePerCost >= breakevenRoas) {
+            productType = productTypeNameGoodPerformer; // Good Performer
+      } else if (convValuePerCost >= breakevenRoas - 1) {
+            productType = productTypeNameMediumPerformer; // Medium Performer
+      } else if (impressions < impressionThreshold) {
+            productType = productTypeNameLowImpressions; // Low Impr
+      } else { 
+            productType = productTypeNamePoorPerformer; // Poor performer
+        }
+        products.push([offer_id, impressions, clicks, cost, conversions, conversionValue, convValuePerCost, productType]);
+        count += 1;
+    }
+    Logger.log(count);
+    return products;
+}
+
+function formatOfferId(offerId) {
+    var expectedFormat = /^shopify_[A-Z]{2}_/; // Regulärer Ausdruck für das erwartete Format
+
+    if (offerId.startsWith("shopify_")) {
+        if (!expectedFormat.test(offerId)) {
+            var parts = offerId.split('_');
+            if (parts.length > 2) {
+                parts[1] = parts[1].toUpperCase(); // Wandelt nur den Ländercode in Großbuchstaben um, falls nötig
+                offerId = parts.join('_');
+            }
+        }
+    }
+    return offerId;
+}
+
+function pushToSpreadsheet(data) {
+    var spreadsheet = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
+    setCustomLabelInSheet(); // Setzt das Custom Label im Sheet
+    var sheet = spreadsheet.getSheetByName('EYL-Izer');
+    var lastRow = sheet.getMaxRows();
+    sheet.getRange('A2:H' + lastRow).clearContent();
+    var start_row = 2;
+    var endRow = start_row + data.length - 1;
+    var range = sheet.getRange('A' + start_row + ':' + 'H' + endRow);
+    if (data.length > 0) {
+        range.setValues(data);
+    }
+    return;
+}
+
+function setCustomLabelInSheet() {
+    var spreadsheet = SpreadsheetApp.openByUrl(SPREADSHEET_URL); // Verwendet die bestehende URL des Spreadsheets
+    var sheet = spreadsheet.getSheetByName('EYL-Izer'); // Ersetzen Sie dies mit dem Namen Ihres Sheets
+    var customLabelHeader = 'custom label ' + selectedCustomLabel;
+    sheet.getRange('H1').setValue(customLabelHeader);
+}
